bbd1fac13809cfc3b86917f764f6262bb7232f7a
quoha <quoha@quoha.com>
2014-10-18 20:59:33 -0500

    twiddling with instruction bits again

--- qoqillol/main.c
+++ qoqillol/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef struct qocfg qocfg;
 typedef struct qotpt qotpt;
@@ -15,6 +16,20 @@ typedef struct qotpt qotpt;
 typedef unsigned short qocell;
 
 typedef long qovmreg;
+
+// stacks
+//   globalVariables
+//   programCall
+// registers
+//   pc
+//   a    accumulator
+//   b    auxiliary accumulator
+//   c    control register
+//   d    address register
+//   p    stack frame pointer/index
+//   g    global variable base pointer
+// core
+//
 typedef struct qovm {
     qocell *pc;
     qocell *startOfCore;
@@ -28,6 +43,8 @@ typedef struct qovm {
     qovmreg c;    // program counter
     qovmreg d;    // computed address of current instruction
     qovmreg p, g; // index registers
+    qocell  globalVariableStack[512];
+    qocell  programCallStack[512];
     qocell  core[1];
 } qovm;
 
@@ -35,13 +52,16 @@ typedef struct qovm {
 //
 // http://www.gtoal.com/languages/bcpl/amiga/bcpl/booting.txt
 //
-// instruction layout of 16 bits
+// instruction layout assuming qocell to be 16 bits.
 //   bits 15..12 are the function bits
-//   bits 11...8 are the op size in bytes (0 to 15)
-//   bit   7     is  the indirection bit
-//   bits  6     is  the G index modification bit
-//   bits  5     is  the P index modification bit
-//   bits  4...0     is  not used
+//   bit  11     is  the D bit, D loaded from following qcell
+//   bit  10     is  the P bit, P to be added to D
+//   bit   9     is  the G bit, G to be added to D
+//   bit   8     is  the I bit, D loaded indirectly from D
+//   bits  7...0 are used in various ways
+//               short address (8 bits, range -127..+128)
+//               short jump    (8 bits, range -127..+128)
+// note that with a 16 bit address, core is limited to 65k cells
 //
 #define QOP_RD_FUNC(op) (((op) & 0xf000) >> 12)
 #define QOP_RD_SIZE(op) (((op) & 0x0f00) >>  8)
@@ -64,6 +84,7 @@ void  qovm_dump(qovm *vm);
 void  qovm_dump_opcode(qovm *vm, qocell *op);
 void  qovm_emit(qovm *vm, qocell op, void *data);
 void  qovm_exec(qovm *vm);
+int   qovm_hex_to_data_le(const char *startOfHex, size_t length, unsigned char *data);
 void  qovm_load_icode(qovm *vm, const char *code);
 void  qovm_reset(qovm *vm);
 const char *qovm_util_op2mnemonic(qocell op);
@@ -110,9 +131,6 @@ void qovm_dump(qovm *vm) {
     printf(".....: endOfCore      %p\n", vm->endOfCore);
 }
 
-void qovm_dump_cell(qocell op) {
-}
-
 void qovm_dump_opcode(qovm *vm, qocell *op) {
     printf("...vm: ------------------------------------\n");
     printf(".....: address        %zu\n", op - vm->core);
@@ -151,8 +169,12 @@ void qovm_emit(qovm *vm, qocell op, void *data) {
     // the instruction may span multiple cells.
     // bump the program counter to account for that.
     //
-    memcpy(vm->endOfCode, data, bytesToCopy);
-    vm->endOfCode += bytesToCopy;
+    if (bytesToCopy) {
+        qocell *cell = data;
+        while (bytesToCopy-- > 0) {
+            *(vm->endOfCode++) = *(cell++);
+        }
+    }
 }
 
 // An instruction is executed as follows.
@@ -197,52 +219,78 @@ void qovm_exec(qovm *vm) {
     vm->pc += opSize;
 }
 
+// should understand endianness. this version is little-endian:
+//
+//   0x0102030405060708  ==>  0x0807060504030201
+//
+int qovm_hex_to_data_le(const char *hex, size_t length, unsigned char *data) {
+    size_t maxLength = 15 * sizeof(qocell);
+    if (length > maxLength) {
+        printf("error: hex data must not exceed 15 cells/(%zu bytes)\n", maxLength);
+        exit(2);
+    }
+
+//    const char *c = hex + length - 1;
+//    int idx;
+//    for (idx = 0; idx < maxLength; idx++) {
+//        data[idx] = 0;
+//        if (c >= hex) {
+//            data[idx] = *c - (isdigit(*c) ? '0' : 'A');
+//            c--;
+//            if (c >= hex) {
+//                data[idx] = (data[idx] << 4) ^ (*c - (isdigit(*c) ? '0' : 'A'));
+//                c--;
+//            }
+//        }
+//    }
+
+    int opSize = 0;
+//    for (idx = 1; idx < 15; idx++) {
+//        if (idx  * sizeof(qocell) <= length) {
+//            opSize++;
+//        } else {
+//            break;
+//        }
+//    }
+
+    return opSize;
+}
+
 void  qovm_load_icode(qovm *vm, const char *code) {
+    printf(".code: %s\n", code);
     unsigned char function     = 0;
     int           indirectBit  = 0;
     int           gIndexModBit = 0;
     int           pIndexModBit = 0;
     const char   *mnemonic     = 0;
-    unsigned long number       = 0;
     int           numNibs      = 0;
     qocell        opSize       = 0;
     qocell        op;
-    unsigned char  data[sizeof(qocell) * 4];
-    unsigned char *byte = data;
+    unsigned char  data[16 * sizeof(qocell)];
 
     while (*code && vm->endOfCode < vm->endOfCore) {
+        if (isspace(*code)) {
+            while (isspace(*code)) {
+                code++;
+            }
+            continue;
+        } else if (*code == ';') {
+            // comment to the end of the line
+            while (*code && !(*code == '\n')) {
+                code++;
+            }
+            if (*code) {
+                code++;
+            }
+        }
+
+        const char *startOfHex = code;
+        while (*code == '0' || *code == '1' || *code == '2' || *code == '3' || *code == '4' || *code == '5' || *code == '6' || *code == '7' || *code == '8' || *code == '9' || *code == 'A' || *code == 'B' || *code == 'C' || *code == 'D' || *code == 'E' || *code == 'F') {
+            code++;
+        }
+        const char *endOfHex = code;
+
         switch (*code) {
-            case '0': case '1': case '2': case '3': case '4':
-            case '5': case '6': case '7': case '8': case '9':
-                if (numNibs++ == 2) {
-                    byte[0] = byte[0] << 4;
-                    numNibs = 0;
-                }
-                byte[0] = byte[0] & (*code - '0');
-                break;
-            case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
-                if (numNibs++ == 2) {
-                    byte[0] = byte[0] << 4;
-                    numNibs = 0;
-                }
-                byte[0] = byte[0] & (*code - '0');
-                break;
-//            case '0': number = (number << 4) + 0x00; numNibs++; break;
-//            case '1': number = (number << 4) + 0x01; numNibs++; break;
-//            case '2': number = (number << 4) + 0x02; numNibs++; break;
-//            case '3': number = (number << 4) + 0x03; numNibs++; break;
-//            case '4': number = (number << 4) + 0x04; numNibs++; break;
-//            case '5': number = (number << 4) + 0x05; numNibs++; break;
-//            case '6': number = (number << 4) + 0x06; numNibs++; break;
-//            case '7': number = (number << 4) + 0x07; numNibs++; break;
-//            case '8': number = (number << 4) + 0x08; numNibs++; break;
-//            case '9': number = (number << 4) + 0x09; numNibs++; break;
-//            case 'A': number = (number << 4) + 0x0a; numNibs++; break;
-//            case 'B': number = (number << 4) + 0x0b; numNibs++; break;
-//            case 'C': number = (number << 4) + 0x0c; numNibs++; break;
-//            case 'D': number = (number << 4) + 0x0d; numNibs++; break;
-//            case 'E': number = (number << 4) + 0x0e; numNibs++; break;
-//            case 'F': number = (number << 4) + 0x0f; numNibs++; break;
             case 'i': indirectBit  = 0x01; break;
             case 'g': gIndexModBit = 0x01; break;
             case 'p': pIndexModBit = 0x01; break;
@@ -254,38 +302,14 @@ void  qovm_load_icode(qovm *vm, const char *code) {
             case 'j': function = 0x05; mnemonic = qovm_util_op2mnemonic(function); break;
             case 't': function = 0x06; mnemonic = qovm_util_op2mnemonic(function); break;
             case 'f': function = 0x07; mnemonic = qovm_util_op2mnemonic(function); break;
-            case ';':
-                // comment to the end of the line
-                while (*code && !(*code == '\n')) {
-                    code++;
-                }
-                if (*code) {
-                    code++;
-                }
-                break;
             default: // ignore all unknown input
                 break;
         }
+
         if (mnemonic) {
-            // opSize is the number of cells of data needed. while we can
-            // have up to 16 cells, we don't. i'm not sure why. maybe something
-            // to do with not wanting to store anything larger than the largest
-            // c-type value? TODO: think on this decision. opSize must always
-            // be in number of opcells to avoid possibility of data alignement
-            // issues.
+            // opSize is the number of cells of data needed.
             //
-            if (numNibs == 0) {
-                opSize = 0x00;
-            } else if (numNibs <= 4) {
-                opSize = 0x01;
-            } else if (numNibs <= 8) {
-                opSize = 0x02;
-            } else if (numNibs <= 12) {
-                opSize = 0x03;
-            } else {
-                // TODO: warn of the truncation?
-                opSize = 0x04;
-            }
+            opSize = qovm_hex_to_data_le(startOfHex, endOfHex - startOfHex, data);
 
             op  = QOP_WR_FUNC(function);
             op ^= QOP_WR_SIZE(opSize);
@@ -293,14 +317,19 @@ void  qovm_load_icode(qovm *vm, const char *code) {
             op ^= QOP_WR_MODG(indirectBit);
             op ^= QOP_WR_MODP(indirectBit);
 
-            printf(".info: icode(%-5s 0x%04x %2d number => %8lu/0x%08lx)\n", mnemonic, op, opSize, number, number);
-            qovm_emit(vm, op, &number);
+            printf(".info: icode(%-5s 0x%04x %2d)\n", mnemonic, op, opSize);
+            if (opSize) {
+                int idx;
+                for (idx = 0; idx < opSize; idx++) {
+                    printf(".....: .........data 0x%02x\n", data[idx]);
+                }
+            }
+            qovm_emit(vm, op, data);
 
             // reset everything to prepare for the next instruction
             //
             function = 0;
             mnemonic = 0;
-            number   = 0;
             numNibs  = 0;
             opSize   = 0;
             indirectBit = gIndexModBit = pIndexModBit = 0;
@@ -347,6 +376,7 @@ const char *qovm_util_op2mnemonic(qocell op) {
 int main(int argc, const char * argv[]) {
     printf(".info: sizeof(char     ) %3lu bytes\n", sizeof(char     ));
     printf(".info: sizeof(short    ) %3lu bytes\n", sizeof(short    ));
+    printf(".info: sizeof(int      ) %3lu bytes\n", sizeof(int      ));
     printf(".info: sizeof(long     ) %3lu bytes\n", sizeof(long     ));
     printf(".info: sizeof(long long) %3lu bytes\n", sizeof(long long));
     printf(".info: sizeof(float    ) %3lu bytes\n", sizeof(float    ));
@@ -354,13 +384,24 @@ int main(int argc, const char * argv[]) {
     printf(".info: sizeof(pointer  ) %3lu bytes\n", sizeof(void *   ));
     printf(".info: sizeof(qocellr  ) %3lu bytes\n", sizeof(qocell   ));
 
+    union {
+        unsigned char data[sizeof(unsigned long)];
+        unsigned long number;
+    } u;
+    u.number = 0x0102030405060708L;
+    int idx;
+    printf(".info: unsigned long 0x%016lx ", u.number);
+    for (idx = 0; idx < sizeof(unsigned long); idx++) {
+        printf(" %02x", u.data[idx]);
+    }
+    printf("\n");
     qovm *vm = qovm_alloc(16 * 1024, 10, 8);
     qovm_dump(vm);
 
-    qovm_load_icode(vm, "0000l 00px EEga FFpgs AAk DEADBEEFCAFEF00Dj CAFE t DDDDf FFl 1913l ; comments welcome");
+    qovm_load_icode(vm, "1234l 00px EEga FFpgs AAk DEADBEEFCAFEF00Dj CAFE t DDDDf FFl 1913l ; comments welcome");
     //qovm_dump(vm);
 
-    int idx;
+    //int idx;
     for (idx = 0; idx < 100; idx++) {
         qovm_exec(vm);
     }
